68c612721c344dab696ac4c14d2f9eaf44d7052a
WakanaYuki <Justin.Pradel@atleris.com>
2021-05-08 23:35:40 +0200

    - Fixed multiple CSS bugs - Removed Sentry switch (because it wasn't working, will probably be implemented again later on)
    [Commit before reimplemtung site in WebAssembly]

---
+++ Themenschaedel/Themenschaedel.Web/Shared/NavbarBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Themenschaedel.Web.Shared
+{
+    public partial class NavbarBase : ComponentBase
+    {
+        private readonly ILogger<NavbarBase> _logger;
+
+        public NavbarBase(ILogger<NavbarBase> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void OnSentryClick()
+        {
+            _logger.LogDebug("LEBUTTON!!!");
+        }
+    }
+}
